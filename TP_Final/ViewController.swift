dbb188f32aa295b2afea8f9aeeb5b11aa216b9da
eleves <eleves@mac404inf-17.local>
2017-10-23 12:09:05 -0400

    version avec commentaires

--- TP_Final/ViewController.swift
+++ TP_Final/ViewController.swift
@@ -1,37 +1,67 @@
-  //----------------------//----------------------
-  import UIKit
-  //----------------------//----------------------
-  class ViewController: UIViewController {
+ //----------------------//----------------------
+ import UIKit
+ //----------------------//----------------------
+ class ViewController: UIViewController {
+    //---
+    /*
+     Déclaration de la constante qui gardera les jeux dans la mémoire
+     */
+    
+    let userDef = UserDefaultManager()
+    //---
+    /*
+     Déclaration de le LABEL qui envoie des messages à l'utilisateur
+     */
     @IBOutlet weak var tempLabel: UILabel!
     //---
+    /*
+     Déclaration de les 5 Image Views
+     */
     @IBOutlet weak var slot_1: UIImageView!
     @IBOutlet weak var slot_2: UIImageView!
     @IBOutlet weak var slot_3: UIImageView!
     @IBOutlet weak var slot_4: UIImageView!
     @IBOutlet weak var slot_5: UIImageView!
     //---
+    /*
+     Déclaration des images qui font les effets de brouillage
+     */
     var card_blur_1: UIImage!
     var card_blur_2: UIImage!
     var card_blur_3: UIImage!
     var card_blur_4: UIImage!
     var card_blur_5: UIImage!
     //---
+    /*
+     Déclaration des les VIEWs contenant les images d'arrière-plan du jeu
+     */
     @IBOutlet weak var bg_1: UIView!
     @IBOutlet weak var bg_2: UIView!
     @IBOutlet weak var bg_3: UIView!
     @IBOutlet weak var bg_4: UIView!
     @IBOutlet weak var bg_5: UIView!
     //---
+    /*
+     Déclaration des les LABELs GARDER
+     */
     @IBOutlet weak var keep_1: UILabel!
     @IBOutlet weak var keep_2: UILabel!
     @IBOutlet weak var keep_3: UILabel!
     @IBOutlet weak var keep_4: UILabel!
     @IBOutlet weak var keep_5: UILabel!
     //---
+    /*
+     Déclaration des les bouton de miser
+     Déclaration des le Label qui montre le credit
+     Déclaration des le label qui montre la mise
+     */
     @IBOutlet weak var dealButton: UIButton!
     @IBOutlet weak var creditsLabel: UILabel!
     @IBOutlet weak var betLabel: UILabel!
     //---
+    /*
+     Déclaration des tableaux que chargeront les cartes du pont
+     */
     var arrOfCardImages: [UIImage]!
     //---
     var arrOfSlotImageViews: [UIImageView]!
@@ -42,43 +72,109 @@
     //---
     var arrOfKeepLabels: [UILabel]!
     //---
+    /*
+     Déclaration de la variable qui gardera vrai de possible de sélectionner la carte ou le faux cas
+     Déclaration de la variable qui gardera le valeur de mise
+     Déclaration de la variable qui gardera le valeur du credit
+     */
     var permissionToSelectCards = false
     var bet = 0
     var credits = 2000
     //---
+    /*
+     Déclaration de la variable que va garder le nombre de chances
+     */
     var chances = 2
     //---
+    /*
+     Déclaration de la constante que va garder le résultat du jeu de poker
+     */
     let pokerHands = PokerHands()
     //---
+    /*
+     Déclaration du tableau que va garder les valuers de les cartes
+     */
     var handToAnalyse = [(0, ""), (0, ""), (0, ""), (0, ""), (0, "")]
     //---
+    /*
+     Déclaration du tableau que va garder le resultat da main
+     */
     var theHand = [(Int, String)]()
     //----------------------//----------------------
     override func viewDidLoad() {
         //---
+        /*
+         Appele la Founction qui menage les credits
+         */
+        manageCredits()
+        //---
         super.viewDidLoad()
         //---
+        /*
+         Appele la Founction qui associe les cartes aux images
+         */
         createCardObjectsFromImages()
         //---
+        /*
+         Appele la Founction qui charge les tableaux
+         */
         fillUpArrays()
         //---
+        
+        /*
+         Appele la Founction qui prépare les animations
+         */
         prepareAnimations(duration: 0.5,
                           repeating: 5,
                           cards: arrOfCardImages)
         //---
+        /*
+         Appele la Founction qui stylise les images
+         */
         stylizeSlotImageViews(radius: 10,
                               borderWidth: 0.5,
                               borderColor: UIColor.black.cgColor,
                               bgColor: UIColor.yellow.cgColor)
         //---
+        /*
+         Appele la Founction qui stylise les images de fond
+         */
+        
         stylizeBackgroundViews(radius: 10,
                                borderWidth: nil,
                                borderColor: UIColor.black.cgColor,
                                bgColor: nil)
         //---
+        /*
+         Instance le valeur du Label credits
+         */
+        creditsLabel.text = "CRÉDITS : \(credits)"
+        //---
+        /*
+         Appele a Founction qui crée les cartes
+         */
         createDeckOfCards()
         //---
+        //if userDef.getValue(theKey: "credits") == 0{
+        //  userDef.setKey(theValue: 2000 as AnyObject, theKey: "credits")
+        //}
+        
     }
+    //----------------------//----------------------
+    /*
+     Founction qui menage les credits dans la memoire
+     */
+    func manageCredits (){
+        if !userDef.doesKeyExist(theKey: "credits"){
+            userDef.setKey(theValue: credits as AnyObject, theKey: "credits")
+        }else{
+            credits = userDef.getValue(theKey: "credits") as! Int
+        }
+    }
+    
+    /*
+     fonction qui crée les cartes
+     */
     //----------------------//----------------------
     func createDeckOfCards() {
         deckOfCards = [(Int, String)]()
@@ -90,6 +186,9 @@
         }
     }
     //----------------------//----------------------
+    /*
+     fonction qui stulise les images
+     */
     func stylizeSlotImageViews(radius r: CGFloat,
                                borderWidth w: CGFloat,
                                borderColor c: CGColor,
@@ -103,6 +202,9 @@
         }
     }
     //----------------------//----------------------
+    /*
+     fonction qui stylise les fond de les images
+     */
     func stylizeBackgroundViews(radius r: CGFloat,
                                 borderWidth w: CGFloat?,
                                 borderColor c: CGColor,
@@ -116,6 +218,9 @@
         }
     }
     //----------------------//----------------------
+    /*
+     fonction qui charge les tableaux
+     */
     func fillUpArrays() {
         arrOfCardImages = [card_blur_1, card_blur_2, card_blur_3, card_blur_4,
                            card_blur_5]
@@ -124,6 +229,9 @@
         arrOfKeepLabels = [keep_1, keep_2, keep_3, keep_4, keep_5]
     }
     //----------------------//----------------------
+    /*
+     fonction qui instance les variables avec des images qui montreront l'effet de mouvement
+     */
     func createCardObjectsFromImages() {
         card_blur_1 = UIImage(named: "blur_1.png")
         card_blur_2 = UIImage(named: "blur_2.png")
@@ -132,6 +240,9 @@
         card_blur_5 = UIImage(named: "blur_4.png")
     }
     //----------------------//----------------------
+    /*
+     fonction qui prépare l'animation
+     */
     func prepareAnimations(duration d: Double,
                            repeating r: Int,
                            cards c: [UIImage]) {
@@ -142,6 +253,9 @@
         }
     }
     //----------------------//----------------------
+    /*
+     fonction qui fait l'animatiion
+     */
     func returnRandomBlurCards(arrBlurCards: [UIImage]) -> [UIImage] {
         var arrToReturn = [UIImage]()
         var arrOriginal = arrBlurCards
@@ -153,6 +267,9 @@
         return arrToReturn
     }
     //----------------------//----------------------
+    /*
+     fonction qui va distribuer les cartes
+     */
     @IBAction func play(_ sender: UIButton) {
         //---
         if bet == 0 {
@@ -190,6 +307,9 @@
                              repeats: false)
     }
     //----------------------//----------------------
+    /*
+     fonction qui montre le main
+     */
     @objc func displayRandomCards() {
         //---
         theHand = returnRandomHand()
@@ -204,6 +324,9 @@
         //---
     }
     //----------------------//----------------------
+    /*
+     Fonction qui prépare la main prochaine
+     */
     func prepareForNextHand() {
         //---
         if chances == 0 {
@@ -219,6 +342,9 @@
         //---
     }
     //----------------------//----------------------
+    /*
+     Fonction qui montre les cartes pour l'utilisateur
+     */
     func displayCards(arrOfCards: [String]) {
         //---
         var counter = 0
@@ -238,10 +364,14 @@
         //---
         if chances == 0 {
             verifyHand(hand: handToAnalyse)
+            manageCredits()
         }
         //---
     }
     //----------------------//----------------------
+    /*
+     Fonction qui retour le tableau avec les cartes pour le fonction displaycards
+     */
     func removeEmptySlotsAndReturnArray() -> [(Int, String)] {
         var arrToReturn = [(Int, String)]()
         for card in handToAnalyse {
@@ -252,6 +382,9 @@
         return arrToReturn
     }
     //----------------------//----------------------
+    /*
+     Fonction qui associe les variables às images
+     */
     func createCards(theHand: [(Int, String)]) -> [String] {
         //---
         let card_1 = "\(theHand[0].0)\(theHand[0].1).png"
@@ -263,6 +396,9 @@
         //---
     }
     //----------------------//----------------------
+    /*
+     Fonction qui va montrer le main
+     */
     func returnRandomHand() -> [(Int, String)] {
         //---
         var arrToReturn = [(Int, String)]()
@@ -277,6 +413,9 @@
         //---
     }
     //----------------------//----------------------
+    /*
+     fonction qui appele la classe qui verifie la main
+     */
     func verifyHand(hand: [(Int, String)]) {
         if pokerHands.royalFlush(hand: hand) {
             calculateHand(times: 250, handToDisplay: "QUINTE FLUSH ROYALE")
@@ -301,12 +440,19 @@
         }
     }
     //----------------------//----------------------
+    /*
+     fonction qui calcule le valeur de la main
+     */
     func calculateHand(times: Int, handToDisplay: String) {
         credits += (times * bet)
         tempLabel.text = handToDisplay
         creditsLabel.text = "CRÉDITS: \(credits)"
+        userDef.setKey(theValue: credits as AnyObject, theKey: "credits")
     }
     //----------------------//----------------------
+    /*
+     Fonction qui vous permet de sauvegarder les cartes désirées
+     */
     @IBAction func cardsToHold(_ sender: UIButton) {
         //---
         if !permissionToSelectCards {
@@ -332,6 +478,9 @@
         }
     }
     //----------------------//----------------------
+    /*
+     Fonction qui gère les cartes sélectionnées
+     */
     func manageSelectedCards(theTag: Int, shouldAdd: Bool) {
         if shouldAdd {
             handToAnalyse[theTag] = theHand[theTag]
@@ -340,6 +489,9 @@
         }
     }
     //----------------------//----------------------
+    /*
+     Fonction qui effectue les mises
+     */
     @IBAction func betButtons(_ sender: UIButton) {
         //---
         if chances <= 1 {
@@ -366,18 +518,26 @@
             betLabel.text = "MISE : \(bet)"
             creditsLabel.text = "CRÉDITS : \(credits)"
             dealButton.alpha = 1.0
+            
         }
         //---
         resetBackOfCards()
         //---
     }
     //----------------------//----------------------
+    /*
+     Fonction qui va turner les cartes
+     */
     func resetBackOfCards() {
         for back in arrOfSlotImageViews {
             back.image = UIImage(named: "bg_1.png")
         }
     }
     //----------------------//----------------------
+    /*
+     Fonction qui va réinitialiser la tentative
+     */
+    
     func resetCards() {
         //---
         for index in 0...4 {
@@ -389,6 +549,9 @@
         //---
         chances = 2
         //---
+    /*
+    Fonction que va recommencer le jeu
+    */
     }
     @IBAction func resetGame(_ sender: UIButton) {
         //---
@@ -396,10 +559,11 @@
             return
         } else {
             if bet != 0 {
-                  return
+                return
             }else {
                 credits = 2000
                 chances = 2
+                userDef.setKey(theValue: credits as AnyObject, theKey: "credits")
                 creditsLabel.text = "CRÉDITS : \(credits)"
                 tempLabel.text = "BONNE CHANCE!"
                 dealButton.alpha = 1.0
@@ -411,5 +575,5 @@
     }
     
     //----------------------//----------------------
-  }
-  //----------------------//----------------------
+ }
+ //----------------------//----------------------
